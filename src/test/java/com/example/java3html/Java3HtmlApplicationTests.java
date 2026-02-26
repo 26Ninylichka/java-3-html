5249626971e868b07f844f7b5becd4b2a371c1c1
26Ninylichka <gmail.smoljarn@>
2026-02-26 17:43:35 +0100

    commit

---
+++ src/test/java/com/example/java3html/Java3HtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java3html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java3HtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
